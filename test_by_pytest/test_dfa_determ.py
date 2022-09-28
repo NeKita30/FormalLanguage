f7b778674c014b45c8a154cba5de9b5955913fbd
nekita <chubarev.nk@phystech.edu>
2022-09-28 21:14:39 +0300

    fixing a little test mistake

--- test_by_pytest/test_dfa_determ.py
+++ test_by_pytest/test_dfa_determ.py
@@ -6,7 +6,6 @@ def test_dfa_determ_one(mocker):
     mocker.patch('sources.DFA.DFA.DFA._DFA__dfs_deleting_unreached')
 
     dfa = DFA.DFA(alphabet='ab', doa_file="test_by_pytest/doa_texts/dfa_test_determ.doa")
-    dfa.translate_to_doa("test_out.doa")
 
     assert set(dfa.states) == {'0', '1', '2', '3', '4', '5', '6', '1_2', '4_6', '5_6', '2_4_6', '4_5_6'}
     assert dfa.transitions['0'] == {'a': ['1_2'], 'b': ['3']}
