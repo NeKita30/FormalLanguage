7df34081f7ae7c78bdf1044e1c74f3d9ba205442
nekita <chubarev.nk@phystech.edu>
2022-09-29 20:52:29 +0300

    work with starts

---
+++ nfa2dfa.py
@@ -0,0 +1,25 @@
+from sources.DFA import NFA, DFA
+import borrowing.doa2graphviz
+import argparse
+import os
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--alphabet", required=False, default=None, type=str)
+parser.add_argument("--nfa_doa", required=True, type=str)
+parser.add_argument("-cdfa", action='store_true')
+parser.add_argument("--dfa_doa", required=False, default="doa_format.doa", type=str)
+parser.add_argument("-graph", action='store_true')
+parser.add_argument("--graph_name", required=False, default="", type=str)
+args = parser.parse_args()
+
+
+dfa = DFA.DFA(alphabet=args.alphabet, doa_file=args.nfa_doa)
+
+if args.cdfa:
+    dfa.make_complete_dfa()
+
+dfa.translate_to_doa(args.dfa_doa)
+
+if args.graph:
+    borrowing.doa2graphviz.make_graphviz(args.dfa_doa, args.graph_name)
--- regex2machine.py
+++ regex2machine.py
@@ -7,15 +7,18 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--regex", required=True, type=str)
 parser.add_argument("--alphabet", required=False, type=str)
 parser.add_argument("-doa", action='store_true')
-parser.add_argument("-one_nka", action='store_true')
+parser.add_argument("-dfa", action='store_true')
+parser.add_argument("-cdfa", action='store_true')
 parser.add_argument("--doa_name", required=False, default="doa_format.doa", type=str)
 parser.add_argument("-graph", action='store_true')
 parser.add_argument("--graph_name", required=False, default="", type=str)
 args = parser.parse_args()
 
 args.doa_name = os.path.join(os.getcwd(), args.doa_name)
-if args.one_nka:
+if args.dfa or args.cdfa:
     machine = DFA.DFA(args.alphabet, args.regex)
+    if args.cdfa:
+        machine.make_complete_dfa()
 else:
     machine = NFA.NFA(args.alphabet, args.regex)
 
