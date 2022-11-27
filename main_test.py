6827a9bb9a06b38d54df83723407d5aa5fe6093d
nekita <chubarev.nk@phystech.edu>
2022-11-27 03:22:23 +0300

    earley ready

--- main_test.py
+++ main_test.py
@@ -1,9 +1,14 @@
 import sources.Grammars as Grm
 
-my_grm = Grm.Grammar("test_cyk_by_pytest/grm_texts/grm_base_check.grm")
+my_grm = Grm.Grammar("Max_grammar.grm")
 # my_grm.convert_to_chomsky_normal_form()
 # my_grm.write_as_grm("test_cyk_by_pytest/grm_texts/grm_base_output_check.grm")
 # print("".join(my_grm.generate_word()))
 
 earley = Grm.Earley(my_grm)
-print(earley.check_word_occurrence('()'))
+with open('test', 'r') as f:
+    for i, line in enumerate(f.readlines()):
+        word, ans = line.strip().split()
+        ans = (ans == '1')
+        my_ans = earley.check_word_occurrence(word)
+        assert my_ans == ans
--- sources/Grammars/Earley.py
+++ sources/Grammars/Earley.py
@@ -3,12 +3,14 @@ from sources.Grammars.constant import EPS
 from sources.Grammars.rule import Rule
 from itertools import chain
 from copy import deepcopy
+from collections import defaultdict
 
 
 class DotNotation:
-    def __init__(self, left: str, rule_ind: int, separate_ind: int, symbol: str, pose: int):
+    def __init__(self, left: str, rule: tuple[str],
+                 separate_ind: int, symbol: str, pose: int):
         self.left = left
-        self.rule_ind = rule_ind
+        self.rule: tuple[str] = rule
         self.separate_ind = separate_ind
         self.symbol = symbol
         self.pose = pose
@@ -20,98 +22,104 @@ class Earley:
         self.base_start = grammar.start
         grammar.add_new_start()
         self.new_start = grammar.start
+        self.lst_symbols = grammar.nonterminals + [EPS] + grammar.alphabet
+        self.to_num = {symbol: i for i, symbol in enumerate(self.lst_symbols)}
 
     def check_word_occurrence(self, word: str):
-        d_set = self.__make_d_sets(word)
-        return d_set[-1][0]
+        d_set_transition = self.__make_d_sets(word)
+        return d_set_transition[self.to_num[self.new_start], (self.base_start,), 1, 0]
 
-    def __make_d_sets(self, word: str) -> list[list[list[DotNotation]]]:
-        to_num = {symbol: i
-                            for i, symbol in enumerate(chain(self.grammar.nonterminals, (EPS,),
-                                                             self.grammar.alphabet))}
-        d_sets: list[list[list[list[DotNotation]]]] = \
-            [[[list()
-               for left in range(len(self.grammar.nonterminals))]
-              for right_first in range(len(to_num) + 1)]
-             for j in range(len(word) + 1)]
-        d_sets[0][
-            to_num[self.base_start]][
-            to_num[self.new_start]].append(DotNotation(self.new_start,
-                                                                 0,
-                                                                 0,
-                                                                 self.base_start,
-                                                                 0))
+    def __make_d_sets(self, word: str) -> dict[tuple[int, tuple[str], int, int], bool]:
+        d_sets: list[list[list[DotNotation]]] = [[list()
+                                                  for _ in self.lst_symbols]
+                                                 for _ in range(len(word) + 1)]
+        d_sets[0][self.to_num[self.base_start]] = [
+            DotNotation(self.new_start,
+                        self.grammar.rules[self.new_start].rights[0],
+                        0,
+                        self.base_start,
+                        0)]
+        d_set_transition: list[dict[tuple[int, tuple[str], int, int], bool]] = \
+            [defaultdict(bool) for _ in range(len(word) + 1)]
+
         for length in range(len(word) + 1):
-            self.__scan(word, d_sets, length, to_num)
-            copy_d_sets = deepcopy(d_sets)
-            # queue = d_sets[ind]
-            # while copy_d_sets != d_sets:
-            #     changes |= self.__predict(d_sets, length, to_num)
-            #     changes |= self.__complete(d_sets, length, to_num)
-        return d_sets[len(word)]
+            queue = self.__make_queue(d_sets[length])
+            while queue:
+                situation = queue.pop(0)
+                self.__predict(situation, d_sets, length, queue, d_set_transition)
+                self.__complete(situation, d_sets, length, queue, d_set_transition)
+            self.__scan(word, d_sets, length, d_set_transition)
+        return d_set_transition[len(word)]
 
-    def __scan(self, word: str, d_sets: list[list[list[list[DotNotation]]]], ind: int,
-               to_num: dict[str, int]):
-        if ind == 0:
+    @staticmethod
+    def __make_queue(d_set: list[list[DotNotation]]):
+        queue = list()
+        for lst in d_set:
+            for situation in lst:
+                queue.append(situation)
+        return queue
+
+    def __scan(self, word: str, d_sets: list[list[list[DotNotation]]], ind: int,
+               d_set_transition: list[dict[tuple[int, tuple[str], int, int], bool]]):
+        if ind == len(word):
             return
-        for nonterm_sets in d_sets[ind - 1][to_num[word[ind - 1]]]:
-            for situation in nonterm_sets:
-                transition = self.grammar.rules[situation.left].rights[situation.rule_ind]
+        if not word[ind] in self.grammar.alphabet:
+            return
+        for situation in d_sets[ind][self.to_num[word[ind]]]:
+            if not d_set_transition[ind + 1][self.to_num[situation.left],
+                                             situation.rule,
+                                             situation.separate_ind + 1,
+                                             situation.pose]:
+                transition = situation.rule
                 new_separate_ind = situation.separate_ind + 1
-                right_first = transition[situation.separate_ind]
-                d_sets[ind][to_num[right_first]][to_num[situation.left]].append(
+                new_right_first = (transition+(EPS,))[new_separate_ind]
+                d_set_transition[ind + 1][self.to_num[situation.left],
+                                          situation.rule,
+                                          situation.separate_ind + 1,
+                                          situation.pose] = True
+                d_sets[ind + 1][self.to_num[new_right_first]].append(
                     DotNotation(situation.left,
-                                situation.rule_ind,
+                                situation.rule,
                                 new_separate_ind,
-                                right_first,
+                                new_right_first,
                                 situation.pose))
 
-    def __predict(self, d_sets: list[list[list[list[DotNotation]]]],
-                  ind: int,
-                  to_num: dict[str, int]):
-        changes = False
-        copy_d_sets = deepcopy(d_sets)
-        for right_first_sets in copy_d_sets[ind][:len(self.grammar.nonterminals)]:
-            for from_letter_sets in right_first_sets:
-                for situation in from_letter_sets:
-                    right_first = situation.symbol
-                    for rule_ind, transition in enumerate(self.grammar.rules[right_first].rights):
-                        changes = True
-                        d_sets[ind][to_num[transition[0]]][to_num[right_first]].append(
-                            DotNotation(right_first, rule_ind, 0, transition[0], ind)
-                        )
-        return changes
+    def __predict(self, situation: DotNotation, d_sets: list[list[list[DotNotation]]],
+                  ind: int, queue: list[DotNotation],
+                  d_set_transition: list[dict[tuple[int, tuple[str], int, int], bool]]):
+        if situation.symbol in self.grammar.nonterminals:
+            for transition in self.grammar.rules[situation.symbol].rights:
+                if not d_set_transition[ind][self.to_num[situation.symbol],
+                                             transition, 0, ind]:
+                    next_symbol = transition[0]
+                    d_sets[ind][self.to_num[next_symbol]].append(
+                        DotNotation(situation.symbol, transition, 0, next_symbol, ind)
+                    )
+                    d_set_transition[ind][self.to_num[situation.symbol],
+                                          transition, 0, ind] = True
+                    queue.append(d_sets[ind][self.to_num[next_symbol]][-1])
 
-    def __complete(self, d_sets: list[list[list[list[DotNotation]]]], ind: int,
-                   to_num: dict[str, int]):
-        changes = False
-        copy_d_sets = deepcopy(d_sets)
-        for first_letter_sets in copy_d_sets[ind][:len(self.grammar.nonterminals) + 1]:
-            for from_letter_sets in first_letter_sets:
-                for situation in from_letter_sets:
-                    left = situation.left
-                    rule_ind = situation.rule_ind
-                    pose = situation.pose
-                    if situation.separate_ind == len(self.grammar.rules[left].rights[rule_ind]) or \
-                            self.grammar.rules[left].rights[rule_ind] == (EPS,):
-                        for up_letter_from_sets in copy_d_sets[pose][to_num[left]]:
-                            for up_situation in up_letter_from_sets:
-                                changes = True
-                                up_left = up_situation.left
-                                up_rule_ind = up_situation.rule_ind
-                                up_separate = up_situation.separate_ind
-                                up_transition = self.grammar.rules[up_left].rights[up_rule_ind]
-                                if up_separate < len(up_transition) - 1:
-                                    up_first_letter = up_transition[up_separate]
-                                else:
-                                    up_first_letter = EPS
-                                up_separate += 1
-                                d_sets[ind][
-                                    to_num[up_first_letter]][
-                                    to_num[up_left]].append(
-                                    DotNotation(up_left,
-                                                up_rule_ind,
-                                                up_situation.separate_ind+1,
-                                                up_first_letter,
-                                                up_situation.pose))
-        return changes
+    def __complete(self, situation: DotNotation, d_sets: list[list[list[DotNotation]]],
+                   ind: int, queue: list[DotNotation],
+                   d_set_transition: list[dict[tuple[int, tuple[str], int, int], bool]]):
+        left, right, pose = situation.left, situation.rule, situation.pose
+        if situation.separate_ind == len(right) or right == (EPS,):
+            for up_situation in d_sets[pose][self.to_num[left]]:
+                if not d_set_transition[ind][self.to_num[up_situation.left],
+                                             up_situation.rule,
+                                             up_situation.separate_ind + 1,
+                                             up_situation.pose]:
+                    d_set_transition[ind][self.to_num[up_situation.left],
+                                          up_situation.rule,
+                                          up_situation.separate_ind + 1,
+                                          up_situation.pose] = True
+                    up_left = up_situation.left
+                    up_rule = up_situation.rule
+                    up_pose = up_situation.pose
+                    up_separate_ind = up_situation.separate_ind + 1
+                    up_next_symbol = (up_rule+(EPS,))[up_separate_ind]
+                    d_sets[ind][self.to_num[up_next_symbol]].append(
+                        DotNotation(up_left, up_rule, up_separate_ind,
+                                    up_next_symbol, up_pose)
+                    )
+                    queue.append(d_sets[ind][self.to_num[up_next_symbol]][-1])
